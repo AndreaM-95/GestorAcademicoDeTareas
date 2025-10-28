815d1315dc509bdefa2552b2836f0532f08b1f06
Andrea Mejia <yamejia088@gmail.com>
2025-10-27 22:07:53 -0500

    [ADD] Documentation with Swagger

--- src/modules/grades/grades.controller.ts
+++ src/modules/grades/grades.controller.ts
@@ -16,37 +16,56 @@ import { Roles } from 'src/common/decorators/roles.decorator';
 import { Role } from 'src/common/enums/roles.enum';
 import { RolesGuard } from 'src/common/guards/roles.guard';
 import { JwtAuthGuard } from 'src/common/guards/jwt.guard';
+import { ApiBearerAuth, ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 
-@Controller('grades')
+@ApiTags('Grades')
+@ApiBearerAuth()
+@Controller('/api/grades')
 @UseGuards(JwtAuthGuard, RolesGuard)
 export class GradesController {
   constructor(private readonly gradesService: GradesService) {}
 
   @Post()
+  @ApiOperation({ summary: 'Asignar una calificación a una tarea' })
+  @ApiResponse({ status: 200, description: 'Calificación creada en la base de datos' })
+  @ApiResponse({ status: 409, description: 'La tarea ya ha sido calificada' })
+  @ApiResponse({ status: 404, description: 'Calificación con ese ID no existe' })
   @Roles(Role.Professor)
   create(@Body() createGradeDto: CreateGradeDto) {
     return this.gradesService.create(createGradeDto);
   }
 
   @Patch(':id')
+  @ApiOperation({ summary: 'Actualiza la calificación de una tarea' })
+  @ApiResponse({ status: 200, description: 'Calificación actualizada en la base de datos' })
+  @ApiResponse({ status: 404, description: 'Calificación con ese ID no existe' })
   @Roles(Role.Professor)
   update(@Param('id', ParseIntPipe) id: number, @Body() updateGradeDto: UpdateGradeDto) {
     return this.gradesService.update(id, updateGradeDto);
   }
 
   @Get('student/:studentId')
+  @ApiOperation({ summary: 'Lista del estudiante con todas sus tareas y sus calificaciones' })
+  @ApiResponse({ status: 200, description: 'Un objeto con tareas y calificaciones' })
+  @ApiResponse({ status: 400, description: 'El estudiante no tiene calificaciones' })
   @Roles(Role.Professor)
   findByStudent(@Param('studentId', ParseIntPipe) studentId: number) {
     return this.gradesService.findByStudent(studentId);
   }
 
   @Get('student/:studentId/average')
+  @ApiOperation({ summary: 'Promedio de las calificaciones de un estudiante' })
+  @ApiResponse({ status: 200, description: 'Un mensaje con el promedio' })
+  @ApiResponse({ status: 400, description: 'El estudiante no tiene calificaciones' })
   @Roles(Role.Professor, Role.Student)
   getStudentAverage(@Param('studentId', ParseIntPipe) studentId: number) {
     return this.gradesService.getStudentAverage(studentId);
   }
 
   @Delete(':id')
+  @ApiOperation({ summary: 'Elimina una calificación de la base de datos' })
+  @ApiResponse({ status: 200, description: 'Calificación eliminada exitosamente en BD' })
+  @ApiResponse({ status: 404, description: 'Calificación con ese ID no existe' })
   @Roles(Role.Professor)
   remove(@Param('id') id: number) {
     return this.gradesService.remove(id);
